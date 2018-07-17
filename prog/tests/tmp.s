3845a5783c1ffe6b4fb04e96ddb7d1a09f07b299
maoyuchaxue <wangjt15@mails.tsinghua.edu.cn>
2018-07-17 14:12:56 +0800

    fixed bug in byte/half-word mem write

--- prog/tests/tmp.s
+++ prog/tests/tmp.s
@@ -2,9 +2,24 @@
     .global _start
 _start:
 nop
-li x2, 12
-lw x1, 0(x2)
-sw x1, 8(x2)
+li s0, 0x800
+li a5, 0xf000
+lbu a5, 0(a5)
+sb a5, -17(s0)
+lui a5, 0x6
+lbu a4, -17(s0)
+sb a4, 0(a5)
+nop
+loop:
+lui a5, 0xf
+addi a5, a5, 4
+lw a5, 0(a5)
+andi a5,a5,15
+beqz a5, loop
+lui a5, 0xf
+lbu a4, -17(s0)
+sb a4, 0(a5)
+nop
 nop
 nop
 nop
