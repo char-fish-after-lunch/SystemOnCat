c44c9e603a602b2cf0f113a22cc171946dbbd3a4
chyyuu <chyyuu@gmail.com>
2018-07-24 14:55:59 +0800

    add setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,1 @@
+export PATH=/media/chyyuu/209365b9-2ba4-4fd2-a937-049603600cd4/thetools/cpu/rv32-syscat-tools/bin:/media/chyyuu/209365b9-2ba4-4fd2-a937-049603600cd4/thetools/cpu/SystemOnCat/verilator:$PATH
