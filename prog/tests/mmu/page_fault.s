464fea1028b3b0219c36d8a843d4f14367326dfc
maoyuchaxue <wangjt15@mails.tsinghua.edu.cn>
2018-07-26 17:43:50 +0800

    fixed bug: cannot read mtval

--- prog/tests/mmu/page_fault.s
+++ prog/tests/mmu/page_fault.s
@@ -22,18 +22,27 @@ sw x11, 0(x10)
 csrw satp, x1
 
 li x4, 97
+li x6, 108
 li x5, 0x4000
 loop:
 sw x4, 0(x5)
 nop
 addi x4, x4, 1
+inner_loop:
+nop
+nop
+beq x4, x6, inner_loop
 addi x5, x5, 4
 nop
 j loop # after execution, 0x5000 in RAM should have been overwritten
 nop
 
 pf_handler:
+csrr x12, mtval
 li x10, 0x2010
+sw x12, 8(x10)
+csrr x12, mcause
+sw x12, 16(x10)
 li x11, 0x00001401 # page 1 -> page 4 -> page 5 (virtual page 4 mapped to physical page 5)
 sw x11, 0(x10)
 sfence.vma
